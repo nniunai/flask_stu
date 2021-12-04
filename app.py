6e10e12c0f7d9a8c24d578abceac387c475b5b0c
testboy <516780019@qq.com>
2021-12-04 16:51:43 +0800

    test

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask,jsonify,url_for
+import config
+
+
+app = Flask(__name__)
+# 加载外部配置项
+app.config.from_object(config)
+
+
+books=[
+    { "id":1,"name":"三国演义" },
+    { "id":2,"name":"水浒传" },
+    { "id":3,"name":"红楼梦" },
+    { "id":4,"name":"西游记" }
+]
+
+
+@app.route("/book/<int:book_id>")
+def book_detail(book_id):
+    print(book_id)
+
+    for book in books:
+        if book_id == book["id"]:
+            return book
+    return f"id位{book_id}找不到"
+
+
+
+@app.route("/book/list")
+def book_list():
+    for book in books:
+        book['url'] = url_for("book_detail",book_id=book['id'])
+    return jsonify(books)
+
+
+
+@app.route('/')
+def hello_world():
+    # return 'Hello 你好啦风格恢复啦啦!'
+    # return {"name":"你好 世界"}
+    return jsonify(books)
+
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ config.py
@@ -0,0 +1,3 @@
+
+# 返回json的时候 解决中文乱码
+JSON_AS_ASCII = False
